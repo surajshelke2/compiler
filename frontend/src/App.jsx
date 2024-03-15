5f0ab80d293a707b50aa75ffdfbc25ed9f8a6f3d
Suraj Shelke <surajshelke332@gmail.com>
2024-03-15 20:13:49 +0530

    firebase auth completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
-import { Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CodeExecution from './Pages/CodeExecution'
+import Auth from './Pages/Auth'
+import LoginForm from './Pages/Authenctication/Login'
+import SignUpForm from './Pages/Authenctication/Signup'
+import { Home } from '@mui/icons-material'
 
 function App() {
 
   return (
  <>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/assesment' element={<CodeExecution/>} />
+      <Route path="/" element={<Auth/>}/>
+      <Route path="/login" element= {<LoginForm/>}></Route>
+      <Route path='/signup'element={<SignUpForm/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
+      
+    </Routes>
 
- <CodeExecution/>
+    </BrowserRouter>
 
  </>
   )
